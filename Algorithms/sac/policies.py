805d4e4c5f62a3a8436378d3d13f2db1a517427d
bluesunb <sunwoong712@gmail.com>
2022-06-20 13:25:56 +0900

    SAC/single sac 지원

--- Algorithms/sac/policies.py
+++ Algorithms/sac/policies.py
@@ -5,7 +5,7 @@ import torch as th
 from torch import nn
 from torch.nn import functional as F
 
-from stable_baselines3.common.policies import BasePolicy, create_sde_features_extractor
+from stable_baselines3.common.policies import BasePolicy
 from stable_baselines3.common.preprocessing import get_action_dim
 from stable_baselines3.common.torch_layers import (
     BaseFeaturesExtractor,
@@ -84,6 +84,7 @@ class SACPolicy(BasePolicy):
         n_critics: int = 2,
         share_features_extractor: bool = True,
         ntb_mode: bool = False,
+        double_sac: bool = False,
     ):
         super(SACPolicy, self).__init__(
             observation_space,
@@ -107,6 +108,7 @@ class SACPolicy(BasePolicy):
         self.actor_net_kwargs = actor_net_kwargs
         self.critic_net_kwargs = critic_net_kwargs
         self.ntb_mode = ntb_mode
+        self.double_sac = double_sac
 
         self.net_args = {
             "observation_space": self.observation_space,
@@ -152,31 +154,35 @@ class SACPolicy(BasePolicy):
 
         if self.share_features_extractor:
             self.critic = self.make_critic(features_extractor=self.actor.features_extractor)
-            self.critic2 = self.make_critic2(features_extractor=self.actor.features_extractor)
+            critic_parameters = [param for name, param in self.critic.named_parameters() if "features_extractor" not in name]
             # Do not optimize the shared features extractor with the critic loss
             # otherwise, there are gradient computation issues
-            critic_parameters = [param for name, param in self.critic.named_parameters() if "features_extractor" not in name]
-            critic2_parameters = [param for name, param in self.critic2.named_parameters() if "features_extractor" not in name]
+            if self.double_sac:
+                self.critic2 = self.make_critic2(features_extractor=self.actor.features_extractor)
+                critic2_parameters = [param for name, param in self.critic2.named_parameters() if "features_extractor" not in name]
         else:
             # Create a separate features extractor for the critic
             # this requires more memory and computation
             self.critic = self.make_critic(features_extractor=None)
-            self.critic2 = self.make_critic2(features_extractor=None)
             critic_parameters = self.critic.parameters()
-            critic2_parameters = self.critic2.parameters()
+            if self.double_sac:
+                self.critic2 = self.make_critic2(features_extractor=None)
+                critic2_parameters = self.critic2.parameters()
 
         # Critic target should not share the features extractor with critic
         self.critic_target = self.make_critic(features_extractor=None)
         self.critic_target.load_state_dict(self.critic.state_dict())
-        self.critic2_target = self.make_critic2(features_extractor=None)
-        self.critic2_target.load_state_dict(self.critic2.state_dict())
-
         self.critic.optimizer = self.optimizer_class(critic_parameters, lr=lr_schedule(1), **self.optimizer_kwargs)
-        self.critic2.optimizer = self.optimizer_class(critic2_parameters, lr=lr_schedule(1), **self.optimizer_kwargs)
+
+        if self.double_sac:
+            self.critic2_target = self.make_critic2(features_extractor=None)
+            self.critic2_target.load_state_dict(self.critic2.state_dict())
+            self.critic2.optimizer = self.optimizer_class(critic2_parameters, lr=lr_schedule(1), **self.optimizer_kwargs)
 
         # Target networks should always be in eval mode
         self.critic_target.set_training_mode(False)
-        self.critic2_target.set_training_mode(False)
+        if self.double_sac:
+            self.critic2_target.set_training_mode(False)
 
     def _get_constructor_parameters(self) -> Dict[str, Any]:
         data = super()._get_constructor_parameters()
@@ -199,6 +205,7 @@ class SACPolicy(BasePolicy):
                 features_extractor_class=self.features_extractor_class,
                 features_extractor_kwargs=self.features_extractor_kwargs,
                 ntb_mode=self.ntb_mode,
+                double_sac=self.double_sac,
             )
         )
         return data
@@ -239,5 +246,6 @@ class SACPolicy(BasePolicy):
         """
         self.actor.set_training_mode(mode)
         self.critic.set_training_mode(mode)
-        self.critic2.set_training_mode(mode)
+        if self.double_sac:
+            self.critic2.set_training_mode(mode)
         self.training = mode
